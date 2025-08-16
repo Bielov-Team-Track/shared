68ea5c8e1e3787ac5dbea13b67219398700e2af6
Denys Bielov <denys.bielov@gmail.com>
2025-08-16 07:42:30 +0100

    - Removed redundant models

--- Shared/Models/Auth/AuthResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace Shared.Models.Auth;
-
-public class AuthResponse
-{
-    public string Token { get; set; } = string.Empty;
-    public string RefreshToken { get; set; } = string.Empty;
-    public DateTime ExpiresAt { get; set; }
-}
