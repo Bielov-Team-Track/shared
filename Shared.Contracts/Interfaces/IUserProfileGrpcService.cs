e36e7f97b2d2408e300350e58fab135d40627ac3
Denys Bielov <denys.bielov@gmail.com>
2025-12-02 09:04:28 +0000

    - Updated dotnet version and removed UserProfileGrpcService

--- Shared.Contracts/Interfaces/IUserProfileGrpcService.cs
+++
@@ -1,8 +0,0 @@
-using Shared.Contracts.Models;
-
-namespace Shared.Contracts.Interfaces;
-
-public interface IUserProfileGrpcService
-{
-    Task<CreateUserProfileResponse> CreateUserProfileAsync(CreateUserProfileRequest request);
-}
--- Shared.Services/Extensions/ServiceCollectionExtensions.cs
+++ Shared.Services/Extensions/ServiceCollectionExtensions.cs
@@ -2,29 +2,16 @@ using Amazon.S3;
 using Amazon.SimpleEmailV2;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Shared.Contracts.Interfaces;
 using Shared.Services.FileStorage;
 using Shared.Services.FileStorage.Intefaces;
 using Shared.Services.Interfaces;
 using Shared.Services.Services;
 using Shared.Services.Services.Interfaces;
-using Shared.Services.UserProfile;
 
 namespace Shared.Services.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddUserProfileGrpcClient(this IServiceCollection services, string eventsServiceUrl)
-    {
-        services.AddSingleton<IUserProfileGrpcService>(provider =>
-        {
-            var logger = provider.GetRequiredService<ILogger<UserProfileGrpcClient>>();
-            return new UserProfileGrpcClient(eventsServiceUrl, logger);
-        });
-
-        return services;
-    }
-
     public static IServiceCollection AddSharedServices(this IServiceCollection services)
     {
         services.AddScoped<IEmailService, EmailService>();
