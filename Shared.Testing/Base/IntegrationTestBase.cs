96c6c292943808f73762a41af393394f68c43081
Denys Bielov <denys.bielov@gmail.com>
2025-12-27 19:55:19 +0000

    - Added shared testing project.

---
+++ Shared.Testing/Base/IntegrationTestBase.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Shared.Testing.Fixtures;
+
+namespace Shared.Testing.Base;
+
+[TestFixture]
+[Category("Integration")]
+public abstract class IntegrationTestBase
+{
+    protected static PostgresFixture PostgresFixture { get; private set; } = null!;
+    protected DatabaseResetter DatabaseResetter { get; private set; } = null!;
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetUpBase()
+    {
+        PostgresFixture = new PostgresFixture();
+        await PostgresFixture.InitializeAsync();
+
+        DatabaseResetter = new DatabaseResetter(PostgresFixture.ConnectionString);
+        await DatabaseResetter.InitializeAsync();
+    }
+
+    [OneTimeTearDown]
+    public async Task OneTimeTearDownBase()
+    {
+        await PostgresFixture.DisposeAsync();
+    }
+
+    [TearDown]
+    public async Task TearDownBase()
+    {
+        await DatabaseResetter.ResetAsync();
+    }
+}
