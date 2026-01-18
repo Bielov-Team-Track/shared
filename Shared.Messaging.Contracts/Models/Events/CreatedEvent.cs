59331cba085109a56101c50a8792cf57e91f07d0
Denys Bielov <denys.bielov@gmail.com>
2026-01-18 21:43:23 +0000

    - intermediate checkpoint

---
+++ Shared.Messaging.Contracts/Models/Events/CreatedEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.Messaging.Contracts.Models.Events
+{
+    public class CreatedEvent
+    {
+        public Guid EventId { get; set; }
+        public required string Name { get; set; }
+        public string? Type { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+    }
+}
