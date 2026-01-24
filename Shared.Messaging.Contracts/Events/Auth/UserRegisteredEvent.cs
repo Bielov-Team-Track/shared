d97e197d5c3ee462aa0739402d23ed7a4b6a07bc
Denys Bielov <denys.bielov@gmail.com>
2026-01-24 20:08:35 +0000

    - Updated interfaces structure and added user registered event.

---
+++ Shared.Messaging.Contracts/Events/Auth/UserRegisteredEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace Shared.Messaging.Contracts.Events.Auth;
+
+public class UserRegisteredEvent
+{
+    public Guid UserId { get; set; }
+    public string Email { get; set; } = string.Empty;
+}
