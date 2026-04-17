c604beed06ad01f70ad9987c69ab60362c81669b
Denys Bielov <denys.bielov@gmail.com>
2026-04-17 13:55:23 +0100

    refactor(shared): split IRepository into IRepository<T,TKey> + Guid alias

--- Shared.DataAccess/Repositories/BaseRepository.cs
+++ Shared.DataAccess/Repositories/BaseRepository.cs
@@ -5,7 +5,7 @@ using Shared.Models;
 
 namespace Shared.DataAccess.Repositories;
 
-public class BaseRepository<T> : IRepository<T> where T : BaseEntity
+public class BaseRepository<T> : IRepository<T> where T : class, IEntity<Guid>
 {
     protected readonly DbContext _context;
     protected readonly DbSet<T> _dbSet;
@@ -16,139 +16,65 @@ public class BaseRepository<T> : IRepository<T> where T : BaseEntity
         _dbSet = context.Set<T>();
     }
 
-    public virtual async Task<T?> GetByIdAsync(Guid id)
-    {
-        return await _dbSet.FindAsync(id);
-    }
+    public virtual async Task<T?> GetByIdAsync(Guid id) => await _dbSet.FindAsync(id);
 
     public virtual async Task<T?> GetByIdAsync(Guid id, params Expression<Func<T, object>>[] includes)
     {
         var query = _dbSet.AsQueryable();
-
-        foreach (var include in includes)
-        {
-            query = query.Include(include);
-        }
-
+        foreach (var include in includes) query = query.Include(include);
         return await query.FirstOrDefaultAsync(e => e.Id == id);
     }
 
-    public virtual async Task<IEnumerable<T>> GetAllAsync()
-    {
-        return await _dbSet.ToListAsync();
-    }
+    public virtual async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
 
     public virtual async Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includes)
     {
         var query = _dbSet.AsQueryable();
-
-        foreach (var include in includes)
-        {
-            query = query.Include(include);
-        }
-
+        foreach (var include in includes) query = query.Include(include);
         return await query.ToListAsync();
     }
 
-    public virtual async Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate)
-    {
-        return await _dbSet.Where(predicate).ToListAsync();
-    }
+    public virtual async Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate) =>
+        await _dbSet.Where(predicate).ToListAsync();
 
     public virtual async Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes)
     {
         var query = _dbSet.AsQueryable();
-
-        foreach (var include in includes)
-        {
-            query = query.Include(include);
-        }
-
+        foreach (var include in includes) query = query.Include(include);
         return await query.Where(predicate).ToListAsync();
     }
 
-    public virtual void Add(T entity)
-    {
-        _dbSet.Add(entity);
-    }
-
-    public virtual void AddRange(IEnumerable<T> entities)
-    {
-        _dbSet.AddRange(entities);
-    }
-
-    public virtual void Update(T entity)
-    {
-        _dbSet.Update(entity);
-    }
-
-    public virtual void Delete(T entity)
-    {
-        _dbSet.Remove(entity);
-    }
+    public virtual void Add(T entity) => _dbSet.Add(entity);
+    public virtual void AddRange(IEnumerable<T> entities) => _dbSet.AddRange(entities);
+    public virtual void Update(T entity) => _dbSet.Update(entity);
+    public virtual void Delete(T entity) => _dbSet.Remove(entity);
 
     public virtual async Task DeleteByIdAsync(Guid id)
     {
         var entity = await Query().Where(r => r.Id == id).FirstOrDefaultAsync();
-        if (entity != null)
-        {
-            _dbSet.Remove(entity);
-        }
+        if (entity != null) _dbSet.Remove(entity);
     }
 
-    public virtual async Task<bool> ExistsAsync(Guid id)
-    {
-        return await _dbSet.AnyAsync(e => e.Id == id);
-    }
+    public virtual async Task<bool> ExistsAsync(Guid id) => await _dbSet.AnyAsync(e => e.Id == id);
 
-    public Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate)
-    {
-        return _dbSet.AnyAsync(predicate);
-    }
+    public Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate) => _dbSet.AnyAsync(predicate);
 
-    public virtual IQueryable<T> Query()
-    {
-        return _dbSet.AsQueryable();
-    }
-
-    public virtual IQueryable<T> QueryNoTracking()
-    {
-        return _dbSet.AsNoTracking();
-    }
+    public virtual IQueryable<T> Query() => _dbSet.AsQueryable();
+    public virtual IQueryable<T> QueryNoTracking() => _dbSet.AsNoTracking();
 
     public virtual async Task<TResult> ExecuteInTransactionAsync<TResult>(Func<Task<TResult>> operation)
     {
         using var transaction = await _context.Database.BeginTransactionAsync();
-        try
-        {
-            var result = await operation();
-            await transaction.CommitAsync();
-            return result;
-        }
-        catch
-        {
-            await transaction.RollbackAsync();
-            throw;
-        }
+        try { var result = await operation(); await transaction.CommitAsync(); return result; }
+        catch { await transaction.RollbackAsync(); throw; }
     }
 
     public virtual async Task ExecuteInTransactionAsync(Func<Task> operation)
     {
         using var transaction = await _context.Database.BeginTransactionAsync();
-        try
-        {
-            await operation();
-            await transaction.CommitAsync();
-        }
-        catch
-        {
-            await transaction.RollbackAsync();
-            throw;
-        }
+        try { await operation(); await transaction.CommitAsync(); }
+        catch { await transaction.RollbackAsync(); throw; }
     }
 
-    public async Task SaveChangesAsync()
-    {
-        await _context.SaveChangesAsync();
-    }
+    public async Task SaveChangesAsync() => await _context.SaveChangesAsync();
 }
--- Shared.DataAccess/Repositories/Interfaces/IRepository.cs
+++ Shared.DataAccess/Repositories/Interfaces/IRepository.cs
@@ -1,11 +1,12 @@
 using System.Linq.Expressions;
+using Shared.Models;
 
 namespace Shared.DataAccess.Repositories.Interfaces;
 
-public interface IRepository<T> where T : class
+public interface IRepository<T, TKey> where T : class, IEntity<TKey>
 {
-    Task<T?> GetByIdAsync(Guid id);
-    Task<T?> GetByIdAsync(Guid id, params Expression<Func<T, object>>[] includes);
+    Task<T?> GetByIdAsync(TKey id);
+    Task<T?> GetByIdAsync(TKey id, params Expression<Func<T, object>>[] includes);
     Task<IEnumerable<T>> GetAllAsync();
     Task<IEnumerable<T>> GetAllAsync(params Expression<Func<T, object>>[] includes);
     Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate);
@@ -14,14 +15,18 @@ public interface IRepository<T> where T : class
     void AddRange(IEnumerable<T> entities);
     void Update(T entity);
     void Delete(T entity);
-    Task DeleteByIdAsync(Guid id);
-    Task<bool> ExistsAsync(Guid id);
+    Task DeleteByIdAsync(TKey id);
+    Task<bool> ExistsAsync(TKey id);
     Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
     IQueryable<T> Query();
     IQueryable<T> QueryNoTracking();
     Task SaveChangesAsync();
 
-    // Transaction support
     Task<TResult> ExecuteInTransactionAsync<TResult>(Func<Task<TResult>> operation);
     Task ExecuteInTransactionAsync(Func<Task> operation);
+}
+
+// Convenience alias — 95% of entities are keyed by Guid
+public interface IRepository<T> : IRepository<T, Guid> where T : class, IEntity<Guid>
+{
 }
