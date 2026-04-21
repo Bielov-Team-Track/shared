4d2cc09b04a316f55bf884da38d7f74ccfc24403
Denys Bielov <denys.bielov@gmail.com>
2026-04-21 17:45:19 +0100

    feat(contracts): add EventRegistrationOpenedEvent

---
+++ Shared.Messaging.Contracts/Events/Events/EventRegistrationOpenedEvent.cs
@@ -0,0 +1,18 @@
+namespace Shared.Messaging.Contracts.Events.Events;
+
+public class EventRegistrationOpenedEvent : INotificationEvent
+{
+    public Guid EventId { get; init; } = Guid.NewGuid();
+    public DateTime Timestamp { get; init; } = DateTime.UtcNow;
+    public Guid UserId => InvitedUserId;
+    public Guid InvitedUserId { get; set; }
+    public Guid InvitationEventId { get; set; }
+
+    public string EventName { get; set; } = string.Empty;
+    public string? EventLocationName { get; set; }
+    public DateTime EventDateTime { get; set; }
+
+    public Guid? InvitedByUserId { get; set; }
+    public string? InviterName { get; set; }
+    public string? InviterPhotoUrl { get; set; }
+}
