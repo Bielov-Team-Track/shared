fc2a3bdf29d9284a06dfde4538d569ec1b393f98
Denys Bielov <denys.bielov@gmail.com>
2026-04-27 22:35:09 +0100

    feat(exceptions): EntityNotFoundException 2-arg ctor accepting ErrorCode

--- Shared/Exceptions/EntityNotFoundException.cs
+++ Shared/Exceptions/EntityNotFoundException.cs
@@ -1,10 +1,16 @@
-﻿using Shared.Enums;
+using Shared.Enums;
 
 namespace Shared.Exceptions
 {
     public class EntityNotFoundException : ExceptionWithStatusAndErrorCodes
     {
-        public EntityNotFoundException(string message) : base(message, System.Net.HttpStatusCode.NotFound, ErrorCodeEnum.EntityNotFound)
+        public EntityNotFoundException(string message)
+            : base(message, System.Net.HttpStatusCode.NotFound, ErrorCodeEnum.EntityNotFound)
+        {
+        }
+
+        public EntityNotFoundException(string message, ErrorCodeEnum errorCode)
+            : base(message, System.Net.HttpStatusCode.NotFound, errorCode)
         {
         }
     }
